c945c19ae1e73460ac3ec738ea7c775a5585d97d
julian cordoba <dragon_4136@hotmail.com>
2020-05-30 21:41:52 -0500

    intermachine cli

---
+++ bin/index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+// modules
+const { version, description } = require('../package.json')
+const Wappalyzer = require('wappalyzer')
+const inquirer = require('inquirer')
+const program = require('commander')
+
+//wappalyzer
+const stack = async (url) => {
+  const wappalyzer = await new Wappalyzer()
+
+  try {
+    await wappalyzer.init()
+
+    const results = await wappalyzer.open(url).analyze()
+
+    console.log(JSON.stringify(results, null, 2))
+  } catch (error) {
+    console.error(error)
+  }
+
+  await wappalyzer.destroy()
+}
+
+program
+  .version(version, '-v --version')
+  .description(description)
+program.parse(process.argv)
+
+//promot command
+inquirer
+  .prompt({
+    name: 'site',
+    message: 'enter url for analyze the tech stack',
+  })
+  .then((answers) => {
+    stack(answers.site)
+  })
---
+++ readme.md
@@ -0,0 +1,17 @@
+# stack analyze
+
+cli tech stack analyze with **node.js** using the wappalyzer module.
+
+use the cli program install
+
+> npm i -g stack-analyze
+mac or linux using sudo, windows using cmd or power as administrador
+
+
+## example code
+> stack-analyze
+> ? enter url for analyze the tech stack http://example.com
+
+
+## author this project Julian David Cordoba Torres as omega5300
+**Intermachine Developers LICENSE MIT**
