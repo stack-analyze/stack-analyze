83039ac7dd7988a493f3e44660144aad692f793e
omega5300 <omegamatrix5351@gmail.com>
2023-09-02 21:55:35 -0500

    version 1.2.6

--- functions/pokemon.js
+++ functions/pokemon.js
@@ -11,34 +11,34 @@ const pokeStats = {
   hp: 255,
   attack: 194,
   defense: 230,
-  'special-attack': 180,
-  'special-defense': 230,
+  "special-attack": 180,
+  "special-defense": 230,
   speed: 200,
   xp: 635
-}
+};
 
 /** 
 *	@param {string}
 * @returns {string}
 */
 const barColor = stat => {
-	switch(stat) {
-		case "hp":
-      return "{bar}".red
-		case "attack":
-		  return "{bar}".yellow
-		case "defense":
-		  return "{bar}".brightYellow
-		case "special-attack":
-		  return "{bar}".blue
-		case "special-defense":
-		  return "{bar}".green
-		case "speed":
-			return "{bar}".magenta
-		default:
-			return "{bar}".cyan
-	}
-}
+  switch(stat) {
+    case "hp":
+      return "{bar}".red;
+    case "attack":
+      return "{bar}".yellow;
+    case "defense":
+      return "{bar}".brightYellow;
+    case "special-attack":
+      return "{bar}".blue;
+    case "special-defense":
+      return "{bar}".green;
+    case "speed":
+      return "{bar}".magenta;
+    default:
+      return "{bar}".cyan;
+  }
+};
 
 /** 
 * @async
@@ -49,12 +49,12 @@ export default async function pokemonInfo(pokemon) {
   console.clear();
   
   const multibar = new CliProgress.MultiBar({
-		format: "{stats} | {bar} | {value}/{total}",
-		clearOnComplete: false,
-		stopOnComplete: true,
-		hideCursor: true,
-		forceRedraw: true,
-	}, CliProgress.Presets.shades_grey);
+    format: "{stats} | {bar} | {value}/{total}",
+    clearOnComplete: false,
+    stopOnComplete: true,
+    hideCursor: true,
+    forceRedraw: true,
+  }, CliProgress.Presets.shades_grey);
   
   try {
     const data = await (
@@ -82,23 +82,23 @@ export default async function pokemonInfo(pokemon) {
     
     const PokeInfo = boxen(info.types, {title: `${id} - ${name}`});
     
-    multibar.create(pokeStats.xp, 0, { stats: 'xp' }, {
-      format: `{stats} | ${barColor('xp')} | {value}/{total}`,
-    }).update(base_experience)
+    multibar.create(pokeStats.xp, 0, { stats: "xp" }, {
+      format: `{stats} | ${barColor("xp")} | {value}/{total}`,
+    }).update(base_experience);
       
     stats.forEach(({base_stat, stat}) => {
       multibar.create(pokeStats[stat.name], 0, { stats: stat.name }, {
         format: `{stats} | ${barColor(stat.name)} | {value}/{total}`,
-      }).update(base_stat)
+      }).update(base_stat);
       
-      info[stat.name] = base_stat
-    })
+      info[stat.name] = base_stat;
+    });
     
-    multibar.log(`${PokeInfo}\n`)
+    multibar.log(`${PokeInfo}\n`);
     
     setTimeout(() => {
-    	multibar.stop()
-    }, 3000)
+      multibar.stop();
+    }, 3000);
     
     stackSave("poke-info.json", JSON.stringify(info, null, 2));
   } catch(err) {
